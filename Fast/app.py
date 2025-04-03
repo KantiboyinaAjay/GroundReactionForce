f2c708cb457c429aeb4ca36721ec6aae9b9fbabc
kantiboyinaAjay <ajaykantiboyina12389@gmail.com>
2025-04-03 23:38:40 +0530

    delete python env

--- Fast/app.py
+++ Fast/app.py
@@ -132,3 +132,6 @@ def retrieve_predictions(request: Request):
     paginated_entries = entries[start:end]
     
     return {"start": start, "end": end, "total": total, "paginated_entries": paginated_entries, "entries": entries}
+
+# if __name__ == "__main__":
+#     uvicorn.run("app:app", host="127.0.0.1", port=8000, reload=True)
